f1ada7bf06a8b42dfbcf6012f388e5fa848803c6
Alexandre Bourget <alex@bourget.cc>
2011-02-15 14:01:35 -0500

    Fix server reload.

--- pyramid_socketio/servereload.py
+++ pyramid_socketio/servereload.py
@@ -7,5 +7,5 @@ def socketio_serve_reload():
     """Spawn a new process and reload when it dies"""
     while True:
         ret = os.system("socketio-serve --watch %s" % (sys.argv[1]))
-        if ret != 3:
+        if ret != 768:
             break
