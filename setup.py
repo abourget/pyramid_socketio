a261b2225abd228c20fe3957fe31754b5600245b
Alexandre Bourget <alex@bourget.cc>
2011-02-15 14:27:19 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import sys
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-#README = open(os.path.join(here, 'README.txt')).read()
-#CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
     'pyramid',
